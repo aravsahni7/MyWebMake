5b2cb5ec5bafad20d0a402b428db75587131f68b
aravsahni7 <aravsahni7@gmail.com>
2025-12-27 02:20:00 -0500

    Update site changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig(() => ({
   plugins: [react()],
-  base: "/",
+  base: "/", // ✅ custom domain
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
